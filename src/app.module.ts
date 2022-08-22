095d74f801baa6060ef3e1e980c80451ffab3ea9
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-21 23:37:58 -0300

    config: typeorm initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.128.3',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    }),
+  ],
 })
 export class AppModule {}
