384d4398b7beb4d099195167d06892fa5ef54929
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-25 01:23:11 -0300

    refactoring redis config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,10 +1,6 @@
 import Redis from 'ioredis';
 import 'dotenv/config';
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
-});
+const redis = new Redis();
 
 export default redis;
