daf5804cb01963ec3c239b5a704e463bf83534ec
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-25 01:52:46 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  //
   SwaggerModule.setup('swagger', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
